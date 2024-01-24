da6ffe474bdb7e116e4720b44c7478adeae5cc3e
Bob Bao <me@baobo.me>
2024-01-24 22:23:41 +0800

    record build message

--- pages/api/dm.ts
+++ pages/api/dm.ts
@@ -1,6 +1,7 @@
 import { emailToUserId, postBlockToChannelId, postToUserId } from '@/lib/slack';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getCache, setCache } from '@/lib/upstash';
+import { postgres } from '@/lib/supabase';
 
 export default async function handler(
   req: NextApiRequest,
@@ -24,7 +25,7 @@ export default async function handler(
 
   console.info('req.body = ', req.body);
 
-  let message = req.body.message as string;
+  const message = req.body.message as string;
   const email = req.body.email as string;
 
   const userId = await getUserId(email.trim());
@@ -35,16 +36,35 @@ export default async function handler(
     });
   }
 
-  if (message.includes("SUCCESS")) {
+  let notification = '';
+  if (message.includes('SUCCESS')) {
     if (email.toLowerCase() === 'pc@moego.pet') {
       return res.status(200).send('');
     }
-    message = `:tada: ${message}`;
+    notification = `:tada: ${message}`;
   } else {
-    message = `:face_holding_back_tears: ${message}`;
+    notification = `:face_holding_back_tears: ${message}`;
   }
 
-  await postToUserId(userId, res, message);
+  const record = extractInfo(message);
+  if (record) {
+    const { data, error } = await postgres.from('build_record').insert([
+      {
+        result: record.result,
+        repository: record.repository,
+        branch: record.branch,
+        sequence: record.sequence,
+        email: email,
+        user_id: userId,
+        text: message,
+      },
+    ]);
+    if (error) {
+      console.log('insert Error:', error);
+    }
+  }
+
+  await postToUserId(userId, res, notification);
 }
 
 async function getUserId(email: string): Promise<string> {
@@ -55,4 +75,23 @@ async function getUserId(email: string): Promise<string> {
     await setCache(email, userId);
   }
   return userId;
+}
+
+function extractInfo(text: string):
+  | {
+      result: string;
+      repository: string;
+      branch: string;
+      sequence: string;
+    }
+  | undefined {
+  const match = text.match(/(BUILD \w+).*» (\w+) » (\w+) #(\d+)/);
+  if (match) {
+    return {
+      result: match[1],
+      repository: match[2],
+      branch: match[3],
+      sequence: match[4],
+    };
+  }
 }
