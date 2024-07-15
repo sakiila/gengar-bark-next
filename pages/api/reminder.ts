1ea98caea150921981afbc11c49a6e84e0d5e4a7
Bob Bao <me@baobo.me>
2024-07-15 16:22:57 +0800

    fix prompt

--- pages/api/reminder.ts
+++ pages/api/reminder.ts
@@ -72,7 +72,7 @@ export default async function handler(
     getGPTResponse3(prompts),
     getDALLEResponse3(
       String(
-        `生成一张图片：活泼有趣地提醒大家${entities[0].type}，不能出现性暗示、暴力、违法犯罪等内容。`,
+        `漫画，幽默，开心，活力，温暖，一只或多只可爱的小动物提醒你做${entities[0].type}的动作`,
       ),
     ),
   ]);
