c41ca6b6aa19bdc8dfae4860807324f2a3f7336d
Bob Bao <me@baobo.me>
2024-11-27 21:33:38 +0800

    🔧 fix(lib/events_handlers/team_join.ts): handle optional chaining for user timezone retrieval

--- lib/events_handlers/team_join.ts
+++ lib/events_handlers/team_join.ts
@@ -10,7 +10,7 @@ export default async function team_join(
   const id = user.id;
   const realName = user.profile.real_name_normalized;
   const email = user.profile.email;
-  const tz = user.tz?? 'Asia/Chongqing';
+  const tz = user?.tz ?? 'Asia/Chongqing';
   const isBot = user.is_bot;
   const deleted = user.deleted;
   const teamId = user.team_id;
