892e79dc9a606e87707961f843b64c661a0032b8
Bob Bao <me@baobo.me>
2025-07-15 22:37:52 +0800

    🚀 refactor(pages/api/whitelist): update link and text in Chinese

--- lib/growthbook/multi-pet.ts
+++ lib/growthbook/multi-pet.ts
@@ -1,87 +1,107 @@
 import { initAppointmentConnection } from '@/lib/database/redshift';
+import { postgres } from '@/lib/database/supabase';
 
 const GB_TOKEN = process.env.GB_TOKEN;
 
-export async function queryMultiPet(): Promise<{
-  shiftManagement: { count: number };
-  calendar: { count: number };
-  allCount: number;
-}> {
+export async function queryMultiPet(): Promise<any[]> {
 
-  const businessIds = await getBusinessIdsFromMultiPet();
+  // const businessIds = await getBusinessIdsFromMultiPet();
 
   try {
     const appointmentDB = await initAppointmentConnection();
 
-    const newBusinessIdsResult = await appointmentDB.query<{ business_id: string }[]>(
-      `select distinct b.id as business_id
+    const newResults = await appointmentDB.query<{
+      business_id: string,
+      company_id: string,
+      owner_email: string,
+      staff_availability_type: number,
+      show_slot_location: number,
+      show_slot_time: string
+    }[]>(
+      `select b.id    as business_id,
+              c.id    as company_id,
+              a.email as owner_email,
+              b.staff_availability_type,
+              cal.show_slot_location,
+              cal.show_slot_time
        from mysql_prod.moe_business.moe_business b
                 left join mysql_prod.moe_business.moe_company c on b.company_id = c.id
                 left join pg_moego_account_prod.public.account a on c.account_id = a.id
-       where b.id = ANY ($1)
-         and a.email not ilike '%moego.pet%' and a.email not ilike '%mymoement.com%'`,
-      [businessIds],
+                left join moe_business.moe_calendar cal on cal.business_id = b.id
+       where b.company_id > 0
+         and c.level > 0
+         and b.app_type in (1, 2)
+         and a.email not ilike '%moego.pet%'
+         and a.email not ilike '%mymoement.com%'`,
     );
 
-    // 提取 business_id 并转换为数字数组
-    const newBusinessIds = newBusinessIdsResult.map(row => Number(row.business_id));
+    const { data: oldResults } = await postgres.from('book_by_slot_watch').select('business_id, company_id, owner_email, staff_availability_type, show_slot_location, show_slot_time');
 
-    console.log("newBusinessIds:", newBusinessIds);
-    console.log("newBusinessIds length:", newBusinessIds.length);
+    // 遍历 newResults 和 oldResults，如果 newResults 中的 business_id 在 oldResults 中不存在，则将 newResults 中的数据插入到 book_by_slot_watch 表中
+    // 由于 oldResults 可能为 null，需要先判断
+    const results = [];
 
-    // 并行执行所有查询
-    const [shiftManagementResult, calendarResult, allCount] = await Promise.all([
-      // 查询开了 Shift management 的 by slot
-      appointmentDB.query<{ count: number }[]>(
-        `select distinct count(id)
-         from moe_business.moe_business
-         where staff_availability_type = 2
-           and id = ANY($1)`,
-        [newBusinessIds],
-      ),
+    if (Array.isArray(oldResults)) {
+      for (const newResult of newResults) {
+        const oldResult = oldResults.find((result: any) => result.business_id === newResult.business_id);
+        if (!oldResult) {
+          await postgres.from('book_by_slot_watch').insert({
+            business_id: newResult.business_id,
+            company_id: newResult.company_id,
+            owner_email: newResult.owner_email,
+            staff_availability_type: newResult.staff_availability_type,
+            show_slot_location: newResult.show_slot_location,
+            show_slot_time: newResult.show_slot_time,
+          });
+        }
+      }
 
-      // 查询开了 Calendar indicator
-      appointmentDB.query<{ count: number }[]>(
-        `select  distinct count(business_id)
-         from moe_business.moe_calendar
-         where show_slot_location = 1
-           and business_id = ANY($1)`,
-        [newBusinessIds],
-      ),
+      // 遍历 oldResults 和 newResults，如果 staff_availability_type 或 show_slot_location 或 show_slot_time 有变化，则查询出来，记录变化前后的数据，并返回
+      for (const oldResult of oldResults) {
+        const newResult = newResults.find((result: any) => result.business_id === oldResult.business_id);
+        if (newResult && (newResult.staff_availability_type !== oldResult.staff_availability_type || newResult.show_slot_location !== oldResult.show_slot_location || newResult.show_slot_time !== oldResult.show_slot_time)) {
+          results.push({
+            oldResult,
+            newResult,
+          });
+        }
+      }
 
-      // 查询 newBusinessIds 数量
-      newBusinessIds.length
-    ]);
+      console.log('results:', results);
 
-    console.log('查询结果:', {
-      shiftManagement: shiftManagementResult[0],
-      calendar: calendarResult[0],
-      allCount: allCount,
-    });
+      // 遍历 results，更新 book_by_slot_watch 表中的数据
+      for (const result of results) {
+        await postgres.from('book_by_slot_watch').update({
+          staff_availability_type: result.newResult.staff_availability_type,
+          show_slot_location: result.newResult.show_slot_location,
+          show_slot_time: result.newResult.show_slot_time,
+          update_time: new Date().toISOString(),
+        }).eq('business_id', result.oldResult.business_id);
+      }
 
-    return {
-      shiftManagement: shiftManagementResult[0],
-      calendar: calendarResult[0],
-      allCount: allCount,
-    };
+
+    }
+    return results;
+
   } catch (error) {
     console.error('查询失败:', error);
     throw error;
   }
+
 }
 
 async function getBusinessIdsFromMultiPet(): Promise<number[]> {
   const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${GB_TOKEN}`);
+  myHeaders.append('Authorization', `Bearer ${GB_TOKEN}`);
 
   const requestOptions: RequestInit = {
-    method: "GET",
+    method: 'GET',
     headers: myHeaders,
-    redirect: "follow" as RequestRedirect
+    redirect: 'follow' as RequestRedirect,
   };
 
   try {
-    const response = await fetch("https://growthbook.moego.pet/growthbook-api/api/v1/features/enable_multi_pet_by_slot", requestOptions);
+    const response = await fetch('https://growthbook.moego.pet/growthbook-api/api/v1/features/enable_multi_pet_by_slot', requestOptions);
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -93,16 +113,16 @@ async function getBusinessIdsFromMultiPet(): Promise<number[]> {
     const productionRules = data.feature?.environments?.production?.rules || [];
 
     //  [ { business: { '$in': [Array] } } ]
-    const conditions = productionRules.map((rule: any) => rule.condition || "{}").map((condition: string) => JSON.parse(condition));
-    console.log("Production rules conditions:", conditions);
+    const conditions = productionRules.map((rule: any) => rule.condition || '{}').map((condition: string) => JSON.parse(condition));
+    console.log('Production rules conditions:', conditions);
 
     const businessIds = conditions.map((condition: any) => condition.business?.['$in'] || []).flat().map(Number);
-    console.log("Business IDs:", businessIds);
+    console.log('Business IDs:', businessIds);
 
     return businessIds;
 
   } catch (err) {
-    console.error("获取 GB 数据失败:", err);
+    console.error('获取 GB 数据失败:', err);
     return [];
   }
 }
--- pages/api/whitelist.ts
+++ pages/api/whitelist.ts
@@ -18,24 +18,25 @@ function calculatePercentage(count: number, total: number): string {
  * @param result 查询结果
  * @returns 格式化的报告文本
  */
-function generateReportText(result: {
-  shiftManagement: { count: number };
-  calendar: { count: number };
-  allCount: number;
-}): string {
-  const currentTime = formatDateToCustomString(new Date());
-  const timezoneOffset = new Date().getTimezoneOffset();
-  const timezoneString = timezoneOffset === 0 ? 'UTC' : `UTC${timezoneOffset > 0 ? '-' : '+'}${Math.abs(timezoneOffset / 60)}`;
-  
-  const shiftManagementPercentage = calculatePercentage(Number(result.shiftManagement.count), result.allCount);
-  const calendarPercentage = calculatePercentage(Number(result.calendar.count), result.allCount);
+function generateReportText(result: any[]): string {
 
-  return `
-:tada: *<https://growthbook.moego.pet/features/enable_multi_pet_by_slot|By Slot 白名单>监控报告*\n 
-截止 ${currentTime} (${timezoneString})，在白名单的 ${result.allCount} 家 Business 中，\n
-启用了 Shift Management by Slot 功能的有 ${result.shiftManagement.count} 家，占比 ${shiftManagementPercentage}%，\n
-启用了 Calendar Indicator 功能的有 ${result.calendar.count} 家，占比 ${calendarPercentage}%。
-  `.trim();
+  // 遍历 result，如果 staff_availability_type 或 show_slot_location 或 show_slot_time 有变化，则拼装变化前后的数据文本
+  // 例如 staff_availability_type 从 1 变为 2，则拼装出：启用了 Shift Management by Slot 功能
+  // 例如 show_slot_location 从 0 变为 1，则拼装出：启用了 Calendar Indicator 功能
+  // 例如 show_slot_time 从 0 变为 1，则拼装出：启用了 Calendar Indicator 功能
+  let changeText = '';
+  for (const item of result) {
+    changeText += `\n${item.newResult.owner_email}(${item.newResult.business_id})：`;
+    if (Number(item.newResult.staff_availability_type) == 2) {
+      changeText += `启用了 Shift Management by Slot 功能`;
+    }
+    if (Number(item.newResult.show_slot_location) == 1 || Number(item.newResult.show_slot_time) == 1) {
+      changeText += `启用了 Calendar Indicator 功能`;
+    }
+
+  }
+
+  return changeText;
 }
 
 export default async function handler(
@@ -56,8 +57,9 @@ export default async function handler(
     // console.log("reportText:", reportText);
 
     // #gr-usages-monitor
-    await postToChannelId('C091YDUP3GU', res, reportText);
-    
+    // await postToChannelId('C091YDUP3GU', res, reportText);
+    await postToChannelId('C067ENL1TLN', res, reportText);
+
     res.status(200).json({ success: true });
   } catch (error) {
     console.error('处理请求失败:', error);
