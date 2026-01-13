f792ba4cdd3d94302f06439cfad277619d3aa137
Bob Bao <me@baobo.me>
2026-01-13 14:18:36 +0800

    🔧 feat(lib/jira): add conditional inclusion of `customfield_11580` for CRM project issues

--- lib/jira/create-issue.ts
+++ lib/jira/create-issue.ts
@@ -223,7 +223,7 @@ export async function createIssue(text: string, channel: string, ts: string, use
     };
   }
 
-  // 只有当 projectKey 为 CRM 时才添加 customfield_11580
+  // 只有当 projectKey 为 CRM 时才添加 customfield_11580 (Feature Domains)
   if ('CRM' == nowProjectKey) {
     requestBody.fields.customfield_11580 = {
       id: '12127', // Clients & pets
